008d769add6f677bdfe695f6b2c687c1e157c289
Luis Blanco <raubtierxxx@gmail.com>
2017-07-06 23:15:17 +0300

    require wip

--- src/index.js
+++ src/index.js
@@ -1,20 +1,36 @@
 'use strict';
 
-const fs   = require('fs');
-const path = require('path');
+const resolve = require('./resolve');
+const Source = require('./source');
+
 
 const libs = [];
 
+const cache = {};
+
 module.exports = {
 	
-	require(file) {
+	require(name) {
 		
-		const resolved = resolve(file, libs);
+		const resolved = resolve(name, libs);
 		
 		if ( ! resolved ) {
-			throw new Error(`Can't locate sources for "${file}".`);
+			throw new Error(`Can't locate sources for "${name}".`);
 		}
 		
+		return resolved.reduce((prev, file) => {
+			const source = new Source(file);
+			if (source.error) {
+				throw new Error(source.error.toString());
+			}
+			if ( ! cache[file] ) {
+				cache[file] = source;
+			}
+			Object.keys(source.compiled).forEach(k => {
+				prev[k] = source.compiled[k];
+			});
+			return prev;
+		}, {});
 		
 	},
 	
